e77ad757ed89f9c28b4d73cfa4bfaf772129b5a6
LIU Dongyuan / 柳东原 <liu.dongyuan@gmail.com>
2015-07-08 22:13:25 -0700

    Initial Commit

---
+++ White/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  White
+//
+//  Created by Dongyuan Liu on 2015-07-08.
+//  Copyright © 2015 Sike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
